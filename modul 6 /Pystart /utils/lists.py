4dc8341b429bf5715cc1cd4a80fe6c78ac887a1d
Emilia-Tet <141513243+Emilia-Tet@users.noreply.github.com>
2024-07-01 12:11:42 +0200

    Delete modul 6 /Pystart /utils directory

--- modul 6 /Pystart /utils/lists.py
+++
@@ -1,9 +0,0 @@
-def add_lists(list1, list2):
-    if len(list2) < len(list1):
-        for i in range(len(list1) - len(list2)):
-            list2.append(0)
-
-    elif len(list2) > len(list1):
-        for i in range(len(list2) - len(list1)):
-            list2.append(0)
-    return [x+y for x, y in zip(list1, list2)]
